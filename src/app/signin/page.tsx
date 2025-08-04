086fb8932628861417382c426181bd841b58a6d3
Ranga Bandara <rangabandara1729@gmail.com>
2025-08-04 11:59:14 +0530

    Add supabase login

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+import { signin } from './actions'
+
+export default function LoginPage() {
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={signin}>Sign in</button>
+    </form>
+  )
+}
